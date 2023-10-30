55871431d589c351b4eb33802dd7c92c600c1560
Brad Babb <brad_babb@cable.comcast.com>
2023-10-30 15:28:43 -0500

    updates

---
+++ lang/go/idiomatic/physical_product/numbers.go
@@ -0,0 +1,17 @@
+package physical_product
+
+type ModelNumber string
+type PartNumber string
+
+// https://en.wikipedia.org/wiki/Stock_keeping_unit
+type StockKeepingUnit string                 // SKU
+type SerialNumber string                     // SN
+type GlobalTradeItemNumber string            // GTIN
+type UniveralProductCode string              // UPC
+type UniversalAgencyBookNumber string        // UABN
+type InternaltionalStandardBookNumber string // ISBN
+
+type Barcode string
+
+// https://en.wikipedia.org/wiki/QR_code
+type QrCode string // Quick Response Code
