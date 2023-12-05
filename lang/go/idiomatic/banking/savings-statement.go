f7c5a287faec95c57b2cd3e6151acd0053b262f7
bbabb <brad.babb@boundedinfinity.com>
2023-12-05 00:06:46 -0600

    update

--- lang/go/idiomatic/banking/savings-statement.go
+++ lang/go/idiomatic/banking/savings-statement.go
@@ -6,9 +6,9 @@ import (
 )
 
 type SavingsStatement struct {
-	Id              id.Id                   `json:"id,omitempty"`
-	Account         BankAccount             `json:"account,omitempty"`
-	BeginningOnDate rfc3339date.Rfc3339Date `json:"begin-on-date,omitempty"`
-	EndOnDate       rfc3339date.Rfc3339Date `json:"end-on-date,omitempty"`
-	StatementDate   rfc3339date.Rfc3339Date `json:"statement-date,omitempty"`
+	Id            id.Id                   `json:"id,omitempty"`
+	Account       BankAccount             `json:"account,omitempty"`
+	BeginOnDate   rfc3339date.Rfc3339Date `json:"begin-on-date,omitempty"`
+	EndOnDate     rfc3339date.Rfc3339Date `json:"end-on-date,omitempty"`
+	StatementDate rfc3339date.Rfc3339Date `json:"statement-date,omitempty"`
 }
