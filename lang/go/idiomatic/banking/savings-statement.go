22802b807b36a0ea07eaeb92e265173fcc81d678
bbabb <brad.babb@boundedinfinity.com>
2023-12-05 00:05:59 -0600

    update

---
+++ lang/go/idiomatic/banking/savings-statement.go
@@ -0,0 +1,14 @@
+package banking
+
+import (
+	"github.com/boundedinfinity/rfc3339date"
+	"github.com/boundedinfinity/schema/idiomatic/id"
+)
+
+type BankingSavingsStatement struct {
+	Id              id.Id                   `json:"id,omitempty"`
+	Account         BankAccount             `json:"account,omitempty"`
+	BeginningOnDate rfc3339date.Rfc3339Date `json:"begin-on-date,omitempty"`
+	EndOnDate       rfc3339date.Rfc3339Date `json:"end-on-date,omitempty"`
+	StatementDate   rfc3339date.Rfc3339Date `json:"statement-date,omitempty"`
+}
