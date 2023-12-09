10ca779981789234cdcfc227b232c58f99a848bb
bbabb <brad.babb@boundedinfinity.com>
2023-12-09 08:11:23 -0600

    update

--- lang/go/idiomatic/currency/internal/main/gen.go
+++ lang/go/idiomatic/currency/internal/main/gen.go
@@ -1,17 +1,123 @@
 package main
 
-// https://www.six-group.com/en/products-services/financial-information/data-standards.html
-// https://www.six-group.com/dam/download/financial-information/data-center/iso-currrency/lists/list-one.xml
-
 import (
 	"encoding/xml"
+	"errors"
 	"fmt"
 	"os"
 	"runtime"
 	"strconv"
 
 	"github.com/boundedinfinity/go-commoner/idiomatic/pather"
+	"github.com/dave/jennifer/jen"
 )
+
+func main() {
+	fmt.Println("ISO Currency Code Generator")
+
+	var iso iso4217
+
+	if err := parse(&iso); err != nil {
+		panic(err)
+	}
+
+	var content string
+
+	if err := generate(iso, &content); err != nil {
+		panic(err)
+	}
+
+	fmt.Println(content)
+}
+
+// /////////////////////////////////////////////////////////////////
+//  Code gen
+// /////////////////////////////////////////////////////////////////
+
+func generate(iso iso4217, content *string) error {
+	typeName := "IsoCurrency"
+	stop := 2
+
+	file := jen.NewFile("main")
+	// file.Type().Id(typeName).String()
+
+	file.Type().Id(typeName).Struct(
+		jen.Id("Code").String(),
+		jen.Id("Country").String(),
+		jen.Id("Name").String(),
+		jen.Id("Number").Int(),
+		jen.Id("DecimalPlaces").Int(),
+	)
+
+	mapper := func(mapName string, fn func(e entry) any) {
+		file.Var().Id(mapName).Op("=").Map(jen.String()).Id(typeName).Values(jen.DictFunc(func(d jen.Dict) {
+			for i, entry := range iso.Table.Entries {
+				if i > stop {
+					break
+				}
+
+				d[jen.Lit(fn(entry))] = jen.Id(entry.Code)
+			}
+		}))
+	}
+
+	mapper("code2Currency", func(e entry) any { return e.Code })
+	mapper("name2Currency", func(e entry) any { return e.Name })
+	mapper("number2Currency", func(e entry) any { return e.Number })
+	mapper("country2Currency", func(e entry) any { return e.Country })
+
+	for i, entry := range iso.Table.Entries {
+		if entry.Code == "" {
+			continue
+		}
+
+		if i > stop {
+			break
+		}
+
+		file.Var().Id(entry.Code).Op("=").Id(typeName).Values(jen.Dict{
+			jen.Id("Code"):    jen.Lit(entry.Code),
+			jen.Id("Number"):  jen.Lit(entry.Number),
+			jen.Id("Country"): jen.Lit(entry.Country),
+		})
+	}
+
+	*content = fmt.Sprintf("%#v", file)
+
+	return nil
+}
+
+// /////////////////////////////////////////////////////////////////
+//  ISO 4217 parsing
+// /////////////////////////////////////////////////////////////////
+
+func parse(iso *iso4217) error {
+	// https://stackoverflow.com/a/70050843
+	_, filename, _, ok := runtime.Caller(0)
+
+	if !ok {
+		return errors.New("can't determine current directory")
+	}
+
+	dir := pather.Files.Dir(filename)
+	xmlFilename := pather.Join(dir, "list-one.xml")
+
+	fmt.Printf("Input XML file: %v\n", xmlFilename)
+	xmlData, err := os.ReadFile(xmlFilename)
+
+	if err != nil {
+		return err
+	}
+
+	if err := xml.Unmarshal(xmlData, iso); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// https://www.six-group.com/en/products-services/financial-information/data-standards.html
+// https://www.six-group.com/dam/download/financial-information/data-center/iso-currrency/lists/list-one.xml
 
 type iso4217 struct {
 	XMLName   xml.Name `xml:"ISO_4217"`
@@ -25,12 +131,12 @@ type table struct {
 }
 
 type entry struct {
-	XMLName            xml.Name   `xml:"CcyNtry"`
-	CountryName        string     `xml:"CtryNm"`
-	CurrencyName       string     `xml:"CcyNm"`
-	CurrencyCode       string     `xml:"Ccy"`
-	CurrencyNumber     int        `xml:"CcyNbr"`
-	CurrencyMinorUnits CcyMnrUnts `xml:"CcyMnrUnts"`
+	XMLName       xml.Name   `xml:"CcyNtry"`
+	Country       string     `xml:"CtryNm"`
+	Name          string     `xml:"CcyNm"`
+	Code          string     `xml:"Ccy"`
+	Number        int        `xml:"CcyNbr"`
+	DecimalPlaces CcyMnrUnts `xml:"CcyMnrUnts"`
 }
 
 type CcyMnrUnts int64
@@ -67,30 +173,4 @@ func (t *CcyMnrUnts) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error 
 	}
 
 	return nil
-}
-
-func main() {
-	fmt.Println("ISO Currency Code Generator")
-
-	// https://stackoverflow.com/a/70050843
-	_, filename, _, _ := runtime.Caller(0)
-	dir := pather.Files.Dir(filename)
-	xmlFilename := pather.Join(dir, "list-one.xml")
-
-	fmt.Printf("Input XML file: %v\n", xmlFilename)
-	xmlData, err := os.ReadFile(xmlFilename)
-
-	if err != nil {
-		panic(err)
-	}
-
-	var iso iso4217
-
-	if err := xml.Unmarshal(xmlData, &iso); err != nil {
-		panic(err)
-	}
-
-	for _, x := range iso.Table.Entries {
-		fmt.Printf("%v\n", x)
-	}
 }
