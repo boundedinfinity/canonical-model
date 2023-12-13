32f4b9d771b2c92b3ac4b757841b94d993b6a64d
Brad Babb <brad_babb@cable.comcast.com>
2023-12-13 11:26:06 -0600

    updates

---
+++ lang/go/idiomatic/specification/genc/genc.go
@@ -0,0 +1,8 @@
+package genc
+
+// U.S. Government by the Geopolitical Entities, Names, and Codes (GENC)
+//      - https://www.fgdc.gov/standards/news/GENC
+// Geopolitical Entities, Names, and Codes (GENC) Standard (2012-11-27)
+//      - https://nsgreg.nga.mil/doc/view?i=2324
+// THE WORLD FACTBOOK : Country Data Codes
+//      - https://www.cia.gov/the-world-factbook/references/country-data-codes/
