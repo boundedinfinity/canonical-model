efab444a1187aba9191d0c3a10985496908773a9
Brad Babb <brad_babb@cable.comcast.com>
2024-04-02 17:11:39 -0500

    updates

--- docs/src/routes/+page.svelte
+++ docs/src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import "../app.css";
-    import { Int, Float, Bool, String } from "./types";
+    import { Int, Float, Bool, String, Date, DateTime, Duration, Time } from "./types";
 </script>
 
 <h1>Canonical Model Specification 2024-04-01</h1>
@@ -12,4 +12,17 @@
     <li><Int /></li>
     <li><Float /></li>
     <li><Bool /></li>
+    <li><Date /></li>
+    <li><DateTime /></li>
+    <li><Time /></li>
+    <li><Duration /></li>
+</ul>
+
+
+<h2>References</h2>
+<ul>
+    <li>https://json-schema.org/</li>
+    <li>https://pkl-lang.org/</li>
+    <li>https://graphql.org/</li>
+    <li>https://avro.apache.org/</li>
 </ul>
--- docs/src/routes/types/index.ts
+++ docs/src/routes/types/index.ts
@@ -2,5 +2,9 @@ import Int from "./type-int.svelte";
 import Float from "./type-float.svelte";
 import Bool from "./type-bool.svelte";
 import String from "./type-string.svelte";
+import Date from "./type-date.svelte";
+import Time from "./type-time.svelte";
+import DateTime from "./type-date-time.svelte";
+import Duration from "./type-duration.svelte";
 
-export { Int, Float, Bool, String };
+export { Int, Float, Bool, String, Date, Time, DateTime, Duration };
