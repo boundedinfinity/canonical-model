7027218501430f49b84d99ada3ad5a7466412ed3
Brad Babb <brad_babb@cable.comcast.com>
2026-01-20 16:50:47 -0600

    updates

--- example/go/idiomatic/financial/amortization-calculator.go
+++ example/go/idiomatic/financial/amortization-calculator.go
@@ -44,13 +44,13 @@ func (this AmortizationCalculator) Calculate() ([]AmortizationItem, error) {
 		return items, err
 	}
 
-	ceil := func(v float64) float64 {
-		return math.Ceil(v*100) / 100
-	}
+	// ceil := func(v float64) float64 {
+	// 	return math.Ceil(v*100) / 100
+	// }
 
-	floor := func(v float64) float64 {
-		return math.Floor(v*100) / 100
-	}
+	// floor := func(v float64) float64 {
+	// 	return math.Floor(v*100) / 100
+	// }
 
 	if this.RoundUpPayment {
 		payment = ceil(payment)
