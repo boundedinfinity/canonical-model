9346786a3e326b137d0c33fb4d024ae5e0b1c932
Brad Babb <brad_babb@cable.comcast.com>
2026-05-11 09:24:06 -0500

    updates

--- go/idiomatic/government/us/state/vechicle/inspection/approval-notice.go
+++ go/idiomatic/government/us/state/vechicle/inspection/approval-notice.go
@@ -3,6 +3,7 @@ package vehicle_inspection
 import (
 	"github.com/boundedinfinity/canonical-model/go/idiomatic/business"
 	"github.com/boundedinfinity/canonical-model/go/idiomatic/measurement/time"
+	"github.com/boundedinfinity/canonical-model/go/idiomatic/payment"
 	"github.com/boundedinfinity/canonical-model/go/idiomatic/person"
 	"github.com/boundedinfinity/canonical-model/go/idiomatic/vehicle"
 )
@@ -19,6 +20,7 @@ type ApprovalNotice struct {
 	InspectionDate          time.Date         `json:"inspection-date,omitempty"`
 	ControlNumber           string            `json:"control-number,omitempty"`
 	OdometerReading         int               `json:"odometer-reading,omitempty"`
+	PaidWith                payment.Payment   `json:"paid-with,omitempty"`
 	BrakeInspected          struct {
 		FrontLeft  bool `json:"front-left,omitempty"`
 		FrontRight bool `json:"front-right,omitempty"`
