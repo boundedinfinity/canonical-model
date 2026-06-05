9f61cbd251130d83be76e696b88a23e1a9ef3781
Brad Babb <brad_babb@cable.comcast.com>
2026-06-05 16:20:15 -0500

    update

--- go/gen/gen.go
+++ go/gen/gen.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/boundedinfinity/canonical-model/go/gen/model"
 	"github.com/boundedinfinity/go-commoner/errorer"
-	o "github.com/boundedinfinity/go-commoner/functional/optioner"
 )
 
 var (
@@ -35,67 +34,4 @@ func (this generator) Generate() (string, error) {
 	var sb strings.Builder
 
 	return sb.String(), nil
-}
-
-func (this generator) sqlTableName(kind Kind) string {
-
-	return ""
-}
-
-func (this generator) processSqlProperty(table *sqlTable, ki int, kind Kind, pi int, property Property) error {
-	var names []o.Option[string]
-	var types []o.Option[string]
-	column := sqlColumn{}
-
-	switch k := property.Kind.(type) {
-	case *FloatKind:
-		column.Name = k.Name
-		column.Type = SqliteTypes.Real.String()
-	case *IntegerKind:
-		column.Name = k.Name
-		column.Type = SqliteTypes.Integer.String()
-	case *StringKind:
-		column.Name = k.Name
-		column.Type = SqliteTypes.Text.String()
-	case *BooleanKind:
-		column.Name = k.Name
-		column.Type = SqliteTypes.Integer.String()
-		// TODO: add CHECK constraint for boolean values
-		// CHECK (column IN (0, 1))
-		// CHECK (column IN (0, 1) OR column IS NULL) -- if optional
-		// CHECK (column IN (0, 1) OR column IS NULL) -- if optional and not null
-		// CHECK (column IN (0, 1)) -- if not optional and not null
-		// CHECK (column IN (0, 1) OR column IS NULL) -- if not optional and nullable
-		// CHECK (column IN (0, 1) OR column IS NULL) -- if optional and nullable
-		// CHECK (column IN (0, 1) OR column IS NULL) -- if optional and nullable
-	case *EnumerationKind:
-		column.Name = k.Name
-		column.Type = SqliteTypes.Text.String()
-		// TODO: add CHECK constraint for enumeration values
-		// CHECK (column IN ('value1', 'value2', ...))
-		// CHECK (column IN ('value1', 'value2', ...) OR column IS NULL) -- if optional
-		// CHECK (column IN ('value1', 'value2', ...)) -- if not optional
-		// CHECK (column IN ('value1', 'value2', ...) OR column IS NULL) -- if optional and nullable
-		// CHECK (column IN ('value1', 'value2', ...)) -- if not optional and not null
-		// CHECK (column IN ('value1', 'value2', ...) OR column IS NULL) -- if not optional and nullable
-	case *LanguageKind:
-		if k.Database.Defined() {
-			names = append(names, k.Database.Get().Name)
-		}
-		names = append(names, k.Name, k.Alias, o.Some(k.Type))
-
-		if k.Database.Defined() {
-			types = append(types, k.Database.Get().Type)
-		}
-		types = append(types, o.Some("TEXT"))
-	case *ReferenceKind:
-	default:
-		errKindFunc("config.kinds[%d].properties[%d]: not supported: %s", ki, pi, property.Kind.GetKind())
-	}
-
-	column.Name = o.FirstOf(names...).Get()
-	column.Type = o.FirstOf(types...).Get()
-	table.Columns = append(table.Columns, &column)
-
-	return nil
 }
