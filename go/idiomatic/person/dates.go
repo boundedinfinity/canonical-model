9610c78e6c8061c17767cb5e7ddab6611b4c09c6
Brad Babb <brad_babb@cable.comcast.com>
2026-06-12 09:23:22 -0500

    updates

--- go/idiomatic/person/dates.go
+++ go/idiomatic/person/dates.go
@@ -2,24 +2,22 @@ package person
 
 import (
 	"github.com/boundedinfinity/canonical-model/go/idiomatic/certification"
-	"github.com/boundedinfinity/rfc3339date"
+	"github.com/boundedinfinity/canonical-model/go/idiomatic/measurement/time"
 )
 
 type Dates struct {
-	BirthDate      rfc3339date.Rfc3339Date `json:"birth-date,omitempty"`
-	DeathDate      rfc3339date.Rfc3339Date `json:"death-date,omitempty"`
-	MarriageDates  []MarriageDates         `json:"marriage-dates,omitempty"`
-	EducationDates []EducationDates        `json:"dducation-dates,omitempty"`
+	BirthDate      time.Date        `json:"birth-date,omitempty"`
+	DeathDate      time.Date        `json:"death-date,omitempty"`
+	MarriageDates  []MarriageDates  `json:"marriage-dates,omitempty"`
+	EducationDates []EducationDates `json:"education-dates,omitempty"`
 }
 
 type MarriageDates struct {
-	Person Person                  `json:"person,omitempty"`
-	Start  rfc3339date.Rfc3339Date `json:"start,omitempty"`
-	End    rfc3339date.Rfc3339Date `json:"end,omitempty"`
+	Person Person         `json:"person,omitempty"`
+	When   time.DateRange `json:"when,omitempty"`
 }
 
 type EducationDates struct {
 	Institution certification.EducationalInstitution `json:"institution,omitempty"`
-	Start       rfc3339date.Rfc3339Date              `json:"start,omitempty"`
-	Graduation  rfc3339date.Rfc3339Date              `json:"graduation,omitempty"`
+	When        time.DateRange                       `json:"when,omitempty"`
 }
