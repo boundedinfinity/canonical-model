6a804f4656e43be7b0c8c87fa4659dc03de552ed
Brad Babb <brad_babb@cable.comcast.com>
2026-06-23 17:30:06 -0500

    updates

--- go/idiomatic/ider/id_test.go
+++ go/idiomatic/ider/id_test.go
@@ -1,13 +1,14 @@
 package ider_test
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/boundedinfinity/canonical-model/go/idiomatic/ider"
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_prefix_name_and_symbol(t *testing.T) {
+func Test_id(t *testing.T) {
 	tcs := []struct {
 		name     string
 		input    ider.Id
@@ -25,4 +26,38 @@ func Test_prefix_name_and_symbol(t *testing.T) {
 			assert.Equal(t, tc.expected, tc.input)
 		})
 	}
+}
+
+func Test_id_json(t *testing.T) {
+	id := ider.New()
+	data, err := json.Marshal(id)
+	assert.NoError(t, err)
+
+	var parsedId ider.Id
+	err = json.Unmarshal(data, &parsedId)
+	assert.NoError(t, err)
+	assert.Equal(t, id, parsedId)
+
+	type test struct {
+		Id   ider.Id `json:"id"`
+		Name string  `json:"name"`
+	}
+
+	s1 := test{Id: id, Name: "n"}
+	data, err = json.Marshal(s1)
+	assert.NoError(t, err)
+
+	var ps1 test
+	err = json.Unmarshal(data, &ps1)
+	assert.NoError(t, err)
+	assert.Equal(t, s1, ps1)
+
+	a1 := []test{{Id: id, Name: "n"}}
+	data, err = json.Marshal(a1)
+	assert.NoError(t, err)
+
+	var pa1 []test
+	err = json.Unmarshal(data, &pa1)
+	assert.NoError(t, err)
+	assert.Equal(t, a1, pa1)
 }
